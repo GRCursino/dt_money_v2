9a0750a180c6a6ef66cf0acf41a431ac26b09cc0
GRCursino <guiiix93@gmail.com>
2023-02-26 23:59:16 -0300

    Completed modal styling

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,10 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
+import * as Dialog from '@radix-ui/react-dialog'
 
 import logoImg from "../../assets/logo.svg"
+import { NewTransactionModal } from "../NewTransactionModal";
+
+{/* Propriedade asChild é para que o trigger nao gere um novo botão, e sim aproveite o botão que esta dentro da tag*/}
 
 export function Header(){
   return (
@@ -9,7 +13,13 @@ export function Header(){
         <HeaderContent>
           <img src={logoImg} alt="" />
 
-          <NewTransactionButton>Nova transação</NewTransactionButton>
+          <Dialog.Root>
+            <Dialog.Trigger asChild>
+              <NewTransactionButton>Nova transação</NewTransactionButton>
+            </Dialog.Trigger>
+
+            <NewTransactionModal />
+          </Dialog.Root>
         </HeaderContent>
       </HeaderContainer>
     </div>
